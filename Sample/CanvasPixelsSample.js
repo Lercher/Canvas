730a0d63aed168729e6bf1a1f256cca4f872dbfb
Martin Lercher <Lercher@singhammer.de>
2015-10-15 01:06:40 +0200

    Better Sample

--- Sample/CanvasPixelsSample.js
+++ Sample/CanvasPixelsSample.js
@@ -9,6 +9,10 @@
 			lc.putPixel(i+j+20, i, 0, i, 0);
 			lc.putPixel(i+j+30, i, 0, 0, i);
 
+			lc.putWhitePixel(i+j+40, i, i, 0, 0);
+			lc.putWhitePixel(i+j+50, i, 0, i, 0);
+			lc.putWhitePixel(i+j+60, i, 0, 0, i);
+
 			lc.putPixel(i+j+10, 255-i, 255  , 255-i, 255-i);
 			lc.putPixel(i+j+20, 255-i, 255-i, 255  , 255-i);
 			lc.putPixel(i+j+30, 255-i, 255-i, 255-i, 255  );
