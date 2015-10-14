bfe587cf61e2da3c33967b8509c490f571d64d64
Martin Lercher <Lercher@singhammer.de>
2015-10-14 23:43:32 +0200

    More pixels in the sampe

--- Sample/CanvasPixelsSample.js
+++ Sample/CanvasPixelsSample.js
@@ -3,10 +3,16 @@
 (function () {
 	var canvas = document.getElementById("c");
 	var lc = lockContext(canvas);
-	for (var i=0; i * 2 < lc.w; i++) {
-		lc.putPixel(i, i*2, 255, 0, 0);
-		lc.putPixel(i, i, 0, 255, 0);
-		lc.putPixel(i*2, i, 0, 0, 255);
+	for (var i=0; i < 256; i++) {
+		for (var j=0; j < 10; j++) {
+			lc.putPixel(i+j+10, i, i, 0, 0);
+			lc.putPixel(i+j+20, i, 0, i, 0);
+			lc.putPixel(i+j+30, i, 0, 0, i);
+
+			lc.putPixel(i+j+10, 255-i, 255  , 255-i, 255-i);
+			lc.putPixel(i+j+20, 255-i, 255-i, 255  , 255-i);
+			lc.putPixel(i+j+30, 255-i, 255-i, 255-i, 255  );
+		}
 	}
 	lc.show();
 }) ();
